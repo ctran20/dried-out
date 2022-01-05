4c74eac1db2bc7dce6f04e7958c7244d4ccb9804
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-01-05 18:21:21 -0500

    Add interactable to non-road tiles

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] bool isPlaceable;
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
+        if(isPlaceable){
+            Debug.Log(transform.name);
+        }
         
     }
 }
