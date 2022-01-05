6c8b371d7aa8ff28c8a47931900d662419c16848
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-01-05 18:08:26 -0500

    Smooth out enemy movement

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f,5f)] float speed = 1f;
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
 
     void Start()
@@ -14,8 +14,17 @@ public class EnemyMover : MonoBehaviour
 
     IEnumerator FollowPath(){
         foreach(Waypoint waypoint in path){
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while(travelPercent < 1f){
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
