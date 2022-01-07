c6360e98b40cdc54464febd2863cbfe93ef67bd7
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-01-07 16:28:25 -0500

    Tower aim for closest enemy

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -6,21 +6,49 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileParticles;
+    [SerializeField] float range = 15f;
     Transform target;
-
-    private void Start()
-    {
-        //target = FindObjectOfType<EnemyMover>().gameObject.transform;
-        target = GameObject.FindGameObjectWithTag("Center").transform;
-    }
 
     private void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    private void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach(Enemy enemy in enemies){
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if(targetDistance < maxDistance){
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
     }
 
     private void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.position);
         weapon.LookAt(target);
+
+        if(targetDistance < range){
+            Attack(true);
+        }
+        else{
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isActive){
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
