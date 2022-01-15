8642882274368bcfd942d4528870360f9ba2bc61
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-01-15 17:52:25 -0500

    Push before split

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] Node node;
+    [SerializeField] Vector2Int gridSize;
 
-    // Start is called before the first frame update
-    void Start()
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+
+    private void Awake()
     {
-        Debug.Log(node.isWalkable);
+        CreateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void CreateGrid(){
+        for(int x = 0; x < gridSize.x; x++){
+            for(int y = 0; y < gridSize.y; y++){
+                Vector2Int coordinates = new Vector2Int(x, y);
+                grid.Add(coordinates, new Node(coordinates, true));
+            }
+        }
     }
 }
