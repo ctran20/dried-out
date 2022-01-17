01bf53ddaeff82d7e19253ba885089de1e59fb48
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-01-17 16:36:00 -0500

    Closed

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Pathfinder : MonoBehaviour
 {
     [SerializeField] Vector2Int startCoordinates;
-    [SerializeField] Vector2Int destinateCoordinates;
+    [SerializeField] Vector2Int destinationCoordinates;
 
     Node startNode;
     Node destinationNode;
@@ -25,14 +25,15 @@ public class Pathfinder : MonoBehaviour
         if(gridManager != null){
             grid = gridManager.Grid;
         }
-
-        startNode = new Node(startCoordinates, true);
-        destinationNode = new Node(destinateCoordinates, true);
     }
 
     private void Start()
     {
+        startNode = gridManager.Grid[startCoordinates];
+        destinationNode = gridManager.Grid[destinationCoordinates];
+
         BreadthFirstSearch();
+        BuildPath();
     }
 
     void ExploreNeighbors(){
@@ -48,6 +49,7 @@ public class Pathfinder : MonoBehaviour
 
         foreach(Node neighbor in neighbors){
             if(!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable){
+                neighbor.connectedTo = currentSearchNode;
                 reached.Add(neighbor.coordinates, neighbor);
                 frontier.Enqueue(neighbor);
             }
@@ -64,9 +66,27 @@ public class Pathfinder : MonoBehaviour
             currentSearchNode = frontier.Dequeue();
             currentSearchNode.isExplored = true;
             ExploreNeighbors();
-            if(currentSearchNode.coordinates == destinateCoordinates){
+            if(currentSearchNode.coordinates == destinationCoordinates){
                 isRunning = false;
             }
         }
+    }
+
+    List<Node> BuildPath(){
+        List<Node> path = new List<Node>();
+        Node currentNode = destinationNode;
+
+        path.Add(currentNode);
+        currentNode.isPath = true;
+
+        while(currentNode.connectedTo != null){
+            currentNode = currentNode.connectedTo;
+            path.Add(currentNode);
+            currentNode.isPath = true;
+        }
+
+        path.Reverse();
+
+        return path;
     }
 }
