6571c6f2f1b12c76438438d0ee5c4fe8446d1d4d
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-01-16 19:45:13 -0500

    Pathfinder

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    [SerializeField] Node currentSearchNode;
+    [SerializeField] Vector2Int startCoordinates;
+    [SerializeField] Vector2Int destinateCoordinates;
+
+    Node startNode;
+    Node destinationNode;
+    Node currentSearchNode;
+
+    Queue<Node> frontier = new Queue<Node>();
+    Dictionary<Vector2Int, Node> reached;
+
     Vector2Int[] directions = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
     GridManager gridManager;
-    Dictionary<Vector2Int, Node> grid;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
 
     // Start is called before the first frame update
     void Awake()
@@ -16,11 +25,14 @@ public class Pathfinder : MonoBehaviour
         if(gridManager != null){
             grid = gridManager.Grid;
         }
+
+        startNode = new Node(startCoordinates, true);
+        destinationNode = new Node(destinateCoordinates, true);
     }
 
     private void Start()
     {
-        ExploreNeighbors();
+        BreadthFirstSearch();
     }
 
     void ExploreNeighbors(){
@@ -31,6 +43,29 @@ public class Pathfinder : MonoBehaviour
 
             if(grid.ContainsKey(neighborCoords)){
                 neighbors.Add(grid[neighborCoords]);
+            }
+        }
+
+        foreach(Node neighbor in neighbors){
+            if(!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable){
+                reached.Add(neighbor.coordinates, neighbor);
+                frontier.Enqueue(neighbor);
+            }
+        }
+    }
+
+    void BreadthFirstSearch(){
+        bool isRunning = true;
+
+        frontier.Enqueue(startNode);
+        reached.Add(startCoordinates, startNode);
+
+        while(frontier.Count > 0 && isRunning){
+            currentSearchNode = frontier.Dequeue();
+            currentSearchNode.isExplored = true;
+            ExploreNeighbors();
+            if(currentSearchNode.coordinates == destinateCoordinates){
+                isRunning = false;
             }
         }
     }
