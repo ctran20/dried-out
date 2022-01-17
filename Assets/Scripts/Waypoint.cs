30b6f639dac8be095ac6024ba4f0b25397dfd29b
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-01-16 20:37:47 -0500

    Working Tower Select

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    [SerializeField] GameObject towerMenu;
     [SerializeField] Tower towerPrefab;
     [SerializeField] bool isPlaceable;
     public bool IsPlaceable{get{return isPlaceable;}}
@@ -11,9 +12,12 @@ public class Waypoint : MonoBehaviour
     private void OnMouseDown()
     {
         if(isPlaceable){
-            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
-            isPlaceable = !isPlaced;
+            towerMenu.SetActive(true);
         }
-        
+    }
+
+    public void PlaceCannon(){
+        bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+        isPlaceable = !isPlaced;
     }
 }
