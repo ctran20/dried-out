a54286749c1b4ef05c8c02444d2c1f0cc1022140
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-01-22 16:38:44 -0500

    Working files reading

--- Assets/Scripts/WaveMaker.cs
+++ Assets/Scripts/WaveMaker.cs
@@ -10,11 +10,17 @@ public class WaveMaker : MonoBehaviour
     [SerializeField] [Range(5, 20)] int poolSize = 5;
     [SerializeField] [Range(0.1f, 30f)] float spawnTimer = 1f;
 
-    [SerializeField] int[] currWaveInfo;
+    [SerializeField] List<int> currWaveInfo;
 
     [SerializeField] int wave;
-    TextAsset textAssetNames;
+    [SerializeField] TextAsset textAssetNames;
     string[] lines;
+
+    private void Start()
+    {
+        ReadTextFile();
+        ReadCurrWaveInfo();
+    }
 
     // Update is called once per frame
     void Update()
@@ -37,15 +43,13 @@ public class WaveMaker : MonoBehaviour
         // 4  5      5   0      0
         // 0  0      0   30     0
 
-        lines = textAssetNames.text.Split(new string[] { "\n" }, StringSplitOptions.None);
+        lines = textAssetNames.text.Split('\n'); 
     }
 
     void ReadCurrWaveInfo(){
-        int i = 0;
-
-        foreach (string num in lines[wave].Split(char.Parse(" "))) {
-            currWaveInfo[i] = Convert.ToInt32(num);
-            i++;
+        foreach (string num in lines[0].Split('\t')) {
+            Debug.Log(num);
+            currWaveInfo.Add(Convert.ToInt32(num));
         };
     }
 }
