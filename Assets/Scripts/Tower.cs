7baafc275d960f45e0deea827607ec592f682ea0
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-02-01 22:02:16 -0500

    Add Upgrade Function

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,7 +4,57 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    [SerializeField] int cost = 75;    
+    [SerializeField] int cost = 75;
+    [SerializeField] int level;
+
+    [SerializeField] GameObject lvl1;
+    [SerializeField] GameObject lvl2;
+    [SerializeField] GameObject lvl3;
+
+    private void Start()
+    {
+        level = 1;
+    }
+
+    public void Recycle(){
+        Bank bank = FindObjectOfType<Bank>();
+        bank.Deposit(45);
+        Destroy(gameObject);
+    }
+
+    public void Upgrade(){
+        Bank bank = FindObjectOfType<Bank>();
+
+        if (bank == null)
+        {
+            return;
+        }
+
+        if (bank.CurrentBalance >= cost)
+        {
+            SwapTower();
+            bank.Withdraw(cost);
+        }
+        else{
+            //Alert insufficient balance
+        }
+    }
+
+    void SwapTower(){
+        switch(level){
+            case 1:
+                lvl1.SetActive(false);
+                lvl2.SetActive(true);
+                break;
+            case 2:
+                lvl2.SetActive(false);
+                lvl3.SetActive(true);
+                break;
+            default:
+                break;
+        }
+        level++;
+    }
 
     public bool CreateTower(Tower tower, Vector3 position){
         Bank bank = FindObjectOfType<Bank>();
