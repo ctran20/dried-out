54aa89b62aa27ab5b8093ffd6b13e9f78aad4cde
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-02-21 22:29:14 -0500

    Working spike towers

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -6,26 +6,38 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int hitPoints = 5;
-
-    [Tooltip("Add amount to hitpoints when enemies die.")]
-    [SerializeField] int difficultyRamp = 2;
     [SerializeField] GameObject armor;
 
     int currentHitPoints = 0;
-
+    bool damaging;
+    bool poisoned;
+    int poisonDamage;
     Enemy enemy;
 
     private void Start()
     {
         enemy = GetComponent<Enemy>();
+        poisonDamage = 0;
+        poisoned = false;
+        damaging = false;
+    }
+
+    private void Update()
+    {
+        if (poisoned && !damaging)
+        {
+            StartCoroutine(Poison());
+        }
     }
 
     // Start is called before the first frame update
     void OnEnable()
     {
         currentHitPoints = hitPoints;
+        poisoned = false;
+        damaging = false;
 
-        if(armor){
+        if (armor){
             armor.SetActive(true);
         }
     }
@@ -44,5 +56,24 @@ public class EnemyHealth : MonoBehaviour
             hitPoints += 1;
             enemy.RewardGold();
         }
+    }
+
+    public void PoisonHit(int damage)
+    {
+        if(damage > 0){
+            poisonDamage = damage;
+            poisoned = true;
+        }else{
+            poisonDamage = 0;
+            poisoned = false;
+        }
+    }
+
+    IEnumerator Poison()
+    {
+        damaging = true;
+        ProcessHit(poisonDamage);
+        yield return new WaitForSeconds(1f);
+        damaging = false;
     }
 }
--- Assets/Scripts/SpikeDamage.cs
+++ Assets/Scripts/SpikeDamage.cs
@@ -4,41 +4,18 @@ using UnityEngine;
 
 public class SpikeDamage : MonoBehaviour
 {
-    bool coolDown;
-    bool entered;
-
-    private void Start()
-    {
-        coolDown = false;
-        entered = false;
-    }
+    [SerializeField] int damage;    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
         {
-            entered = true;
+            other.GetComponent<EnemyHealth>().PoisonHit(damage);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        entered = false;
-    }
-
-    private void Update()
-    {
-        if (!coolDown && entered)
-        {
-            //TakeDamage(5f);
-            StartCoroutine(Damage());
-        }
-    }
-
-    IEnumerator Damage()
-    {
-        coolDown = false;
-        yield return new WaitForSeconds(0.5f);
-        coolDown = true;
+        other.GetComponent<EnemyHealth>().PoisonHit(-1);
     }
 }
